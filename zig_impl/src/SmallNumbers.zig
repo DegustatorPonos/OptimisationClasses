2ef4b2d15f7e953a85e09e85573cd8b520c55210
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2025-10-23 15:08:16 +0500

    Small numbers class

---
+++ zig_impl/src/SmallNumbers.zig
@@ -0,0 +1,45 @@
+const math = std.math;
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const expect = std.testing.expect;
+
+
+pub const ISN = struct {
+    const machineEpsilon: f64 = 1e-16;
+    const machineZero : f64 = 1e-308;
+
+    bf: *const fn(f64)f64,
+
+    pub fn Init(fx: *const fn(f64)f64) ISN {
+        return ISN {
+            .bf = fx
+        };
+    }
+
+    pub fn Create(a: std.mem.Allocator, fx: *const fn(f64)f64) !*ISN {
+        const outp = try a.create(ISN);
+        outp.* = Init(fx);
+        return outp;
+    }
+
+    pub fn IsISN (self: ISN) bool {
+        // (lim f(x); x -> 0) = 0
+        return @abs(self.bf(machineZero)) == 0;
+    }
+};
+
+fn isnFunc(x: f64) f64 {
+    return math.pow(f64, x, 121);
+}
+
+fn notIsnFunc(x: f64) f64 {
+    return math.pow(f64, x, 2) + 1;
+}
+
+// Tests the capability of identifying infinitely small functions
+test "IsIsn" {
+    const passingISN = ISN.Init(isnFunc);
+    try expect(passingISN.IsISN());
+    const failingISN = ISN.Init(notIsnFunc);
+    try expect(!failingISN.IsISN());
+}
--- zig_impl/src/main.zig
+++ zig_impl/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const math = std.math;
 const fov = @import("FOV.zig").FOV;
+const isn = @import("SmallNumbers.zig").ISN;
 
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
 // const lib = @import("optim_zig_lib");
@@ -10,9 +11,9 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    const alloc = gpa.allocator();
-    var one_var_func = try fov.Create(alloc, baseFx);
-    defer alloc.destroy(one_var_func);
+    const allocator = gpa.allocator();
+    var one_var_func = try fov.Create(allocator, baseFx);
+    defer allocator.destroy(one_var_func);
 
     try stdout.print("F'(1) = {d}\n", .{one_var_func.Df_right(1, 1e-10)});
     try stdout.print("Expected F'(1) = {d}\n", .{derivedFx(1)});
@@ -29,4 +30,5 @@ fn derivedFx(x: f64) f64 {
 // Linking tests from FOV class impl
 test {
     _ = fov;
+    _ = isn;
 }
